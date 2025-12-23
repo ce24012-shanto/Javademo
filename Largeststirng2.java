187c21fa3e7389734f80b01d8a6f95bf8e911883
ce24012-shanto <ce24012@mbstu.ac.bd>
2025-12-23 21:27:33 +0600

    My first commit

---
+++ Largeststirng2.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class LargeststringPrint {
+    static void main() {
+        Scanner input=new Scanner(System.in);
+        String[] arr={"aaaa","apple","banana","mango"};
+        String largest=arr[0];
+        for (int i = 1; i <arr.length; i++) {
+            if(largest.compareTo(arr[i])<0){
+
+                largest=arr[i];
+
+
+            }
+
+
+
+
+        }
+        System.out.println("The largest String is : "+largest);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
